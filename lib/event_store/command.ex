8423c635b79adfaf2e02ff08dbc2d5be75077bf6
Neil Menne <nhm@neilmenne.com>
2017-10-12 11:31:36 -0400

    cleanup + first step towards holistic approach to commands/events

--- lib/event_store/command.ex
+++ lib/event_store/command.ex
@@ -1,30 +1,5 @@
 defmodule EventStore.Command do
-  @moduledoc """
-  TODO: put best words here
-  """
+  @moduledoc false # for now
 
-  # @type t :: [command: String.t,
-  #             entity_id: HLClock.Timestamp.t,
-  #             sequence: integer,
-  #             data: map]
   defstruct [:type, :entity_id, :sequence, :data]
-
-  @callback handle_command(Command.t) :: any
-
-  defmacro __using__(_opts) do
-    quote do
-      @behaviour unquote(__MODULE__)
-
-      def handle_command(_), do: {:error, :unhandled_command}
-      defoverridable [handle_command: 1]
-    end
-  end
-
-  defmacro command(name, module) do
-    quote do
-      def eval_command(%{command: unquote(name)} = command) do
-        unquote(module).handle_command(command)
-      end
-    end
-  end
 end
